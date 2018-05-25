898423b399a48d715308217a7e414ac5beacadc9
kwiecien <k.d.kwiecinski@gmail.com>
2018-05-25 23:57:59 +0200

    feat: Add AsyncTask test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -9,12 +9,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isEmptyString;
 
 @RunWith(AndroidJUnit4.class)
@@ -23,6 +26,19 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldFetchData() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+        new EndpointAsyncTask(new EndpointAsyncTask.OnTaskCompleted() {
+            @Override
+            public void onTaskCompleted(String joke) {
+                assertThat(joke, not(isEmptyString()));
+                signal.countDown();
+            }
+        }).execute();
+        signal.await();
+    }
 
     @Test
     public void shouldDisplayJoke() {
